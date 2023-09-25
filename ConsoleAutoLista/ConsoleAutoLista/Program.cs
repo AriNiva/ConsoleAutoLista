6bf169c444681592a3e706b602992a11777d4ce9
AriNiva <145963185+AriNiva@users.noreply.github.com>
2023-09-25 10:27:42 +0300

    Add files via upload

---
+++ ConsoleAutoLista/ConsoleAutoLista/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAutoLista
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string auto = "";
+            List<string> Autot = new List<string>();
+            Console.Write("Anna autojen merkit tai x lopettaaksesi: ");
+            while (auto.ToUpper() != "X") 
+            {
+                Console.Write("Auton merkki: ");
+                auto = Console.ReadLine();
+                if (auto.ToUpper() != "X") 
+                { 
+                    Autot.Add(auto);
+                }
+            }
+
+            Autot.Sort();
+
+            Console.WriteLine("Tallensit nämä autot: (yhteensä {0} kappaletta):", Autot.Count);
+            
+            for (int j = 0; j < Autot.Count; j++) 
+            {
+                Console.WriteLine(Autot[j]);
+            }
+            
+            string ekaAuto = Autot.First();
+            Console.WriteLine("Listan ensimmäinen auto => " +  ekaAuto);
+            string vikaAuto = Autot.Last();
+            Console.WriteLine("Listan viimeinen auto => " +  vikaAuto);
+            Console.ReadLine();
+        }
+    }
+}
